a08da3353c4f7eb1c0481c5e35220e379f9e8ee1
Yourr <you@example.com>
2026-04-24 20:04:35 +0530

    sdecwhn

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello from Spring Boot on Kubernetes!";
+        return "hbskgfghv sdhho!";
     }
     
     @GetMapping("/health")
